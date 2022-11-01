ba6bc821519c4dcecde5cfc4ca3e44cda1573ae2
Satvik Pendem <satvik.pendem@gmail.com>
2022-11-01 12:27:28 -0400

    Update clean_db.sh with more options

--- scripts/clean_db.sh
+++ scripts/clean_db.sh
@@ -1,10 +1,16 @@
 # SQL to select all databases created by specific user
-sudo -u postgres psql -o /tmp/db.txt -c "SELECT datname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE rolname = 'newsletter';"
+# -t only tuple
+# -A output not unaligned
+# -q quiet
+# -X Don't run .psqlrc file
+# -o output file
+# -c SQL command
+sudo -u postgres psql -qtAX -o /tmp/db.txt -c "SELECT datname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE rolname = 'newsletter';"
 
 # Drop all databases from db.txt
 while read p; do
     # Disconnect from database
-    sudo -u postgres psql -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='$p';"
+    sudo -u postgres psql -qtAX -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='$p';"
     # Drop database
     sudo -u postgres dropdb $p
 done < /tmp/db.txt
