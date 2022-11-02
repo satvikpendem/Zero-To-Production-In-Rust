e68f7a03d6a23291d1995b5bbe32a3b93bc924e8
Satvik Pendem <satvik.pendem@gmail.com>
2022-11-02 09:53:36 -0400

    Added command line argument to clean_db.sh

--- scripts/clean_db.sh
+++ scripts/clean_db.sh
@@ -6,17 +6,17 @@
 # -o output file
 # -c SQL command
 
-sudo -u postgres psql -qtAX -o /tmp/db.txt -c "SELECT datname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE rolname = 'newsletter';"
+sudo -u postgres psql -qtAX -o /tmp/db.txt -c "SELECT datname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE rolname = '$1';"
 # echo after sudo asks for password for cleaner output
-echo "Selecting..."
+echo "Selecting databases with role `$1`..."
 
 echo "Dropping..."
 # Drop all databases from db.txt
 while read db; do
     # Disconnect from database
     sudo -u postgres psql -qtAX -c "SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname='$db';"
-    # Drop database
     
+    # Drop database
     echo "\t $db..."
     sudo -u postgres dropdb $db
 done < /tmp/db.txt
