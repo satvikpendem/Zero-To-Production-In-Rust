01fddc2acb782cbcef4fcda4a36ce0f7732ddf3a
Satvik Pendem <satvik.pendem@gmail.com>
2022-11-10 13:18:43 -0500

    Finish chapter 6, reject invalid subscribers #1

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -7,12 +7,22 @@ use serde::Deserialize;
 use sqlx::{query, PgPool};
 use uuid::Uuid;
 
-use crate::domain::{NewSubscriber, SubscriberName};
+use crate::domain::{NewSubscriber, SubscriberEmail, SubscriberName};
 
 #[derive(Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
+}
+
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(form: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(form.name)?;
+        let email = SubscriberEmail::parse(form.email)?;
+        Ok(Self { email, name })
+    }
 }
 
 /// Subscribe someone to the newsletter
@@ -27,14 +37,8 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: Form<FormData>, pool: Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let subscriber = NewSubscriber {
-        name,
-        email: form.0.email,
+    let Ok(subscriber) = form.0.try_into()  else {
+        return HttpResponse::BadRequest().finish()
     };
 
     // insert_subscriber(&subscriber, &pool)
@@ -61,7 +65,7 @@ pub async fn insert_subscriber(
             VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        subscriber.email,
+        subscriber.email.as_ref(),
         subscriber.name.as_ref(),
         Utc::now()
     )
