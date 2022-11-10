e7ef3ff6cb482a9bfb02bacfbc3fa9ead411e161
Satvik Pendem <satvik.pendem@gmail.com>
2022-11-10 11:13:41 -0500

    Generalize database cleaning

--- scripts/clean_database.sh
+++ scripts/clean_database.sh
@@ -6,11 +6,16 @@
 # -o output file
 # -c SQL command
 
+RAND=$(sudo cat /proc/sys/kernel/random/uuid | sed 's/[-]//g' | head -c 32) # 32 due to there being 32 characters in the string before a newline
+TEMP=/tmp/database_$RAND.txt
+
 echo "Beginning cleaning..."
 
-sudo -u postgres psql -qtAX -o /tmp/database.txt -c "SELECT datname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE rolname = '$1';"
+sudo -u postgres psql -qtAX -o $TEMP -c "SELECT datname FROM pg_database JOIN pg_authid ON pg_database.datdba = pg_authid.oid WHERE rolname = '$1';"
+
 # echo after sudo asks for password for cleaner output
-echo "Selecting databases with role $($1)..."
+echo "Storing results in\n\t$TEMP"
+echo "Selecting databases with role $1..."
 
 echo "Dropping..."
 # Drop all databases from database.txt
@@ -21,9 +26,12 @@ while read database; do
     # Drop database
     echo "\t $database..."
     sudo -u postgres dropdb $database
-done </tmp/database.txt
+done <$TEMP
 
-echo "Removing..."
-sudo rm /tmp/database.txt
+echo "Checking if file exists\n\t$TEMP..."
+if [ -f "$TEMP" ]; then
+    echo "Removing\n\t$TEMP..."
+    sudo rm $TEMP
+fi
 
 echo "Done"
