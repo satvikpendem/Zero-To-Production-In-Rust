16b3c3f53925c7da722b974b8a29f487f6357101
Satvik Pendem <satvik.pendem@gmail.com>
2023-01-04 12:02:04 -0500

    Finish 7.7.5: Refactor confirmation link logic

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -4,7 +4,7 @@ use secrecy::ExposeSecret;
 use sqlx::{Executor, PgPool};
 use std::io::{sink, stdout};
 use uuid::Uuid;
-use wiremock::MockServer;
+use wiremock::{MockServer, Request};
 use zero2prod::{
     configuration::{self, DatabaseSettings},
     startup::{get_connection_pool, Application},
@@ -32,6 +32,11 @@ pub struct TestApp {
     pub port: u16,
 }
 
+pub struct ConfirmationLinks {
+    pub html: reqwest::Url,
+    pub plain_text: reqwest::Url,
+}
+
 impl TestApp {
     pub async fn post_subscriptions(&self, body: String) -> Response {
         reqwest::Client::new()
@@ -41,6 +46,32 @@ impl TestApp {
             .send()
             .await
             .expect("Failed to execute request.")
+    }
+
+    pub fn get_confirmation_links(&self, email_request: &Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+        // Extract the link from one of the request fields.
+        let get_link = |s: &str| {
+            let links: Vec<_> = linkify::LinkFinder::new()
+                .links(s)
+                .filter(|l| *l.kind() == linkify::LinkKind::Url)
+                .collect();
+            assert_eq!(links.len(), 1);
+
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+
+            confirmation_link.set_port(Some(self.port)).unwrap();
+
+            confirmation_link
+        };
+
+        let html = get_link(body["HtmlBody"].as_str().unwrap());
+        let plain_text = get_link(body["TextBody"].as_str().unwrap());
+
+        ConfirmationLinks { html, plain_text }
     }
 }
 
